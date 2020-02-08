b6ba786f77a341baa7e882a52b8143bb560bcf45
k-nasa <htilcs1115@gmail.com>
2020-02-08 19:51:52 +0900

    feat: all.mdを作れるようにする
    すべてのtextを1つのファイルで管理できるようにするため

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 fmt:
 	prettier --write **/*.md
+all_text:
+	rm ./all.md && ruby make_all.rb
---
+++ make_all.rb
@@ -0,0 +1,22 @@
+all_file = File.open('./all.md', 'w+')
+
+paths = [
+  "./src/SUMMARY.md",
+  "./src/ch00-00-preface.md",
+  "./src/ch01-00.md",
+  "./src/ch01-01-what_is_async_processing.md",
+  "./src/ch01-02-why_async_processing_is_requeired.md",
+  "./src/ch01-03-concurrency_parallelism.md",
+  "./src/ch01-04-conclusion.md",
+  "./src/ch02-00.md",
+  "./src/ch03-00.md",
+  "./src/ch04-00-conclusion.md"
+]
+
+paths.each do |path|
+  f = File.open(path, 'r')
+
+  text = f.read
+
+  all_file.puts(text)
+end
